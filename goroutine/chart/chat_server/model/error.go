1eb065e6c4e562a8a5d333106328d45664d1ba03
JayceQ <16831024@qq.com>
2018-12-02 16:15:38 +0800

    TCP programming and chart_server simple implementation..

--- goroutine/chart/chat_server/model/error.go
+++ goroutine/chart/chat_server/model/error.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	ErrUserNotExist  = errors.New("user not exist")
-	ErrInvalidPasswd = errors.New("Passwd or username not right")
-	ErrInvalidParams = errors.New("Invalid params")
+	ErrInvalidPasswd = errors.New("passwd or username not right")
+	ErrInvalidParams = errors.New("invalid params")
 	ErrUserExist     = errors.New("user exist")
 )
