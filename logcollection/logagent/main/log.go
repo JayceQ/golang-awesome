290d54b9eb85f6f7d908e7277901ba6d9aa82922
JayceQ <16831024@qq.com>
2018-12-09 21:30:54 +0800

    logagnet log and tail module

---
+++ logcollection/logagent/main/log.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego/logs"
+)
+
+func convertLogLevel(level string) int{
+	switch (level){
+	case "debug":
+		return logs.LevelDebug
+	case "warn" :
+		return logs.LevelWarn
+	case "info" :
+		return logs.LevelInfo
+	case "trace" :
+		return logs.LevelTrace
+	}
+
+	return logs.LevelDebug
+}
+
+func initLogger()(err error){
+
+	config := make(map[string]interface{})
+	config["filename"] = appConfig.logPath
+	config["level"] = convertLogLevel(appConfig.logLevel)
+
+	bytes, err := json.Marshal(config)
+
+	if err != nil {
+		fmt.Println("initLogger failed, marshal err:",err)
+		return
+	}
+
+	logs.SetLogger(logs.AdapterFile,string(bytes))
+	return
+}
---
+++ logcollection/logagent/tail/tail.go
@@ -0,0 +1,11 @@
+package tail
+
+
+type CollectConf struct {
+	LogPath string
+	Topic   string
+}
+
+func InitTail(conf []CollectConf)(err error){
+	return
+}
