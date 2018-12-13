02e8114b4f5874f8683b19a02057fd6903debdb6
JayceQ <16831024@qq.com>
2018-12-13 23:27:06 +0800

    etcd clientv3 put and get

---
+++ logcollection/etcd/main/main.go
@@ -0,0 +1,41 @@
+package main
+
+
+import (
+	"context"
+	"fmt"
+	"time"
+	"go.etcd.io/etcd/clientv3"
+)
+
+func main() {
+
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		fmt.Println("connect failed, err:", err)
+		return
+	}
+
+	fmt.Println("connect succ")
+	defer cli.Close()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	_, err = cli.Put(ctx, "/logagent/conf/", "sample_value")
+	cancel()
+	if err != nil {
+		fmt.Println("put failed, err:", err)
+		return
+	}
+	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+	resp, err := cli.Get(ctx, "/logagent/conf/")
+	cancel()
+	if err != nil {
+		fmt.Println("get failed, err:", err)
+		return
+	}
+	for _, ev := range resp.Kvs {
+		fmt.Printf("%s : %s\n", ev.Key, ev.Value)
+	}
+}
