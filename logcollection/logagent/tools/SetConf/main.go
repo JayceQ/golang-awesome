34cc846b79027dc1a154ea877c4c83a7d9f63823
JayceQ <16831024@qq.com>
2018-12-15 23:18:26 +0800

    etcd clientv3 put and get tools

---
+++ logcollection/logagent/tools/SetConf/main.go
@@ -0,0 +1,41 @@
+package SetConf
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"time"
+)
+
+const (
+	EtcdKey = "/badme/backend/logagent/config/172.16.0.4"
+)
+
+func SetLogConfToEtcd() {
+	client, e := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if e != nil {
+		fmt.Println("connect etcd failed, err:", e)
+	}
+
+	fmt.Println("connect success")
+	defer client.Close()
+
+	timeout, cancelFunc := context.WithTimeout(context.Background(), time.Second)
+	response, e := client.Get(timeout, EtcdKey)
+	cancelFunc()
+	if e != nil {
+		fmt.Println("get failed, err:", e)
+		return
+	}
+
+	for _, ev := range response.Kvs {
+		fmt.Printf("%s : %s\n", ev.Key, ev.Value)
+	}
+}
+
+func main() {
+	SetLogConfToEtcd()
+}
