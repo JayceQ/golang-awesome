0de3202c233dc1972fd2212f47d64419b5c6d494
JayceQ <16831024@qq.com>
2018-12-23 22:21:26 +0800

    test elasticsearch consumer

---
+++ logcollection/elasticsearch/main/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	elastic "gopkg.in/olivere/elastic.v5"
+)
+
+type Tweet struct {
+	User    string
+	Message string
+}
+
+func main() {
+	client, err := elastic.NewClient(elastic.SetSniff(false), elastic.SetURL("http://182.61.137.53:9200/"))
+	if err != nil {
+		fmt.Println("connect es error", err)
+		return
+	}
+
+	fmt.Println("conn es succ")
+
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+
+	tweet := Tweet{User: "olivere", Message: "Take Five"}
+	_, err = client.Index().
+		Index("twitter").
+		Type("tweet").
+		Id("1").
+		BodyJson(tweet).
+		Do(ctx)
+	if err != nil {
+		// Handle error
+		panic(err)
+		return
+	}
+	cancelFunc()
+	fmt.Println("insert succ")
+}
