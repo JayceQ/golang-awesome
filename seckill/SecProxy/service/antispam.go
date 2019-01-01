3815fc462022d859924bdfab50b9a5a344241814
JayceQ <16831024@qq.com>
2019-01-01 23:23:48 +0800

    Add ip/uid blacklist to redis queue for anti-spam, load the list to the memory while the program start, use redis command 'BLPOP' to block the black list. Every request has its request and result channel,result chan marshal and enqueue to the redis request queue, the SecLayer pop from the queue, handle request,package the result enqueue to the receive queue , and SecProxy pop the receive queue, result chan block its own result.

--- seckill/SecProxy/service/antispam.go
+++ seckill/SecProxy/service/antispam.go
@@ -1,10 +1,80 @@
 package service
 
-import "sync"
+import (
+	"fmt"
+	"github.com/astaxie/beego/logs"
+	"sync"
+)
 
 type SecLimitMgr struct {
 	UserLimitMap map[int]*Limit
-	IpLimitMap map[string]Limit
+	IpLimitMap map[string]*Limit
 	lock sync.Mutex
 }
 
+func antiSpam(req *SecRequest)( err error){
+	_,ok := secKillConf.idBlackMap[req.UserId]
+	if ok {
+		err = fmt.Errorf("invalid request")
+		logs.Error("userId[%d] is block by id black",req.UserId)
+		return
+	}
+
+	_,ok = secKillConf.ipBlackMap[req.ClientAddr]
+	if ok {
+		err = fmt.Errorf("invalid request")
+		logs.Error("userId[%d] ip[%s] id block by ip black",req.UserId,req.ClientAddr)
+		return
+	}
+
+	secKillConf.secLimitMgr.lock.Lock()
+	//uid 频率控制，防刷
+	limit,ok := secKillConf.secLimitMgr.UserLimitMap[req.UserId]
+	if !ok {
+		limit = &Limit{
+			secLimit: &SecLimit{},
+			minLimit: &MinLimit{},
+		}
+
+		secKillConf.secLimitMgr.UserLimitMap[req.UserId] = limit
+	}
+	secIdCount := limit.secLimit.Count(req.AccessTime.Unix())
+	minIdCount := limit.minLimit.Count(req.AccessTime.Unix())
+
+	//ip 频率控制
+	limit, ok = secKillConf.secLimitMgr.IpLimitMap[req.ClientAddr]
+	if !ok {
+		limit = &Limit{
+			secLimit:&SecLimit{},
+			minLimit:&MinLimit{},
+		}
+		secKillConf.secLimitMgr.IpLimitMap[req.ClientAddr] = limit
+	}
+
+	secIpCount := limit.secLimit.Count(req.AccessTime.Unix())
+	minIpCount := limit.minLimit.Count(req.AccessTime.Unix())
+
+	secKillConf.secLimitMgr.lock.Unlock()
+
+	if secIpCount > secKillConf.AccessLimitConf.IPSecAccessLimit {
+		err = fmt.Errorf("invalid request")
+		return
+	}
+
+	if minIpCount > secKillConf.AccessLimitConf.IPMinAccessLimit {
+		err = fmt.Errorf("invalid request")
+		return
+	}
+
+	if secIdCount > secKillConf.AccessLimitConf.UserSecAccessLimit {
+		err = fmt.Errorf("invalid request")
+		return
+	}
+
+	if minIdCount > secKillConf.AccessLimitConf.UserMinAccessLimit {
+		err = fmt.Errorf("invalid request")
+		return
+	}
+
+	return
+}
---
+++ seckill/SecProxy/service/redis_proc.go
@@ -0,0 +1,67 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/astaxie/beego/logs"
+	"github.com/garyburd/redigo/redis"
+	"time"
+)
+
+func WriteHandle(){
+	for{
+		req := <-secKillConf.SecReqChan
+		conn := secKillConf.proxy2LayerRedisPool.Get()
+
+		data ,err := json.Marshal(req)
+		if err !=nil{
+			logs.Error("json marshal failed, err: %v, req %v",err,req)
+		}
+
+		_, err = conn.Do("LPUSH", "sec_queue", string(data))
+		if err!= nil{
+			logs.Error("lpush failed, err: %v",err)
+			conn.Close()
+		}
+		conn.Close()
+	}
+}
+
+func ReadHandle(){
+	for {
+		conn := secKillConf.proxy2LayerRedisPool.Get()
+		reply, err := conn.Do("RPOP", "recv_queue")
+		data, err := redis.String(reply, err)
+		if err!= nil {
+			time.Sleep(time.Second)
+			conn.Close()
+			continue
+		}
+
+		logs.Debug("rpop from recv_queue success, data: %v",string(data))
+		if err != nil{
+			logs.Error("rpop failed, err %v",err)
+			continue
+		}
+
+		var result SecResult
+		err = json.Unmarshal([]byte(data),&result)
+
+		if err != nil {
+			logs.Error("json Unmarshal failed, err :%v",err)
+			continue
+		}
+		userKey := fmt.Sprintf("%s_%s", result.UserId, result.ProductId)
+
+		secKillConf.UserConnMapLock.Lock()
+		resultChan, ok := secKillConf.UserConnMap[userKey]
+		secKillConf.UserConnMapLock.Unlock()
+		if !ok {
+			conn.Close()
+			logs.Warn("user not found: %v",userKey)
+			continue
+		}
+		resultChan <- &result
+		conn.Close()
+	}
+}
