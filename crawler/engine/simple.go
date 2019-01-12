a92987b50d154fc4a833d14ef8781187e20e5cc8
JayceQ <16831024@qq.com>
2019-01-12 22:47:25 +0800

    Breadth-First Search, test a crawler

--- crawler/engine/simple.go
+++ crawler/engine/simple.go
@@ -1,1 +1,33 @@
 package engine
+
+import (
+	"github.com/gpmgo/gopm/modules/log"
+)
+// 单任务版引擎
+type SimpleEngine struct {
+
+}
+func (e *SimpleEngine) Run(queue ...Request) {
+	var count = 0
+	for len(queue) > 0 {
+		r := queue[0]
+		queue = queue[1:]
+
+		results, err := Worker(r)
+		if err != nil {
+			continue
+		}
+		for _,r := range results.Requests {
+			if IsDuplicate(r.Url) {
+				continue
+			}
+			queue = append(queue, r)
+		}
+		//queue = append(queue, results.Requests...)
+		for _, item := range results.Items {
+			count++
+			log.Warn("Got Item: $%d %v",count, item)
+		}
+	}
+}
+
