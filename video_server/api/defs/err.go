1aa8f7a83915c0ce9b73c753d8a2190576b3993a
JayceQ <16831024@qq.com>
2019-02-03 21:45:49 +0800

    video on demand

--- video_server/api/defs/err.go
+++ video_server/api/defs/err.go
@@ -1,18 +1,18 @@
 package defs
 
 type Err struct {
-	Error string `json:"error"`
+	Error     string `json:"error"`
 	ErrorCode string `json:"error_code"`
 }
 
 type ErrResponse struct {
 	HttpSC int
-	Error Err
+	Error  Err
 }
 
 var (
 	ErrorRequestBodyParseFailed = ErrResponse{HttpSC: 400, Error: Err{Error: "Request body is not correct", ErrorCode: "001"}}
-	ErrorNotAuthUser = ErrResponse{HttpSC: 401, Error: Err{Error: "User authentication failed.", ErrorCode: "002"}}
-	ErrorDBError = ErrResponse{HttpSC: 500, Error: Err{Error: "DB ops failed", ErrorCode: "003"}}
-	ErrorInternalFaults = ErrResponse{HttpSC: 500, Error: Err{Error: "Internal service error", ErrorCode: "004"}}
+	ErrorNotAuthUser            = ErrResponse{HttpSC: 401, Error: Err{Error: "User authentication failed.", ErrorCode: "002"}}
+	ErrorDBError                = ErrResponse{HttpSC: 500, Error: Err{Error: "DB ops failed", ErrorCode: "003"}}
+	ErrorInternalFaults         = ErrResponse{HttpSC: 500, Error: Err{Error: "Internal service error", ErrorCode: "004"}}
 )
