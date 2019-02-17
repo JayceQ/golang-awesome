931f36070d3bde3856036540a16e454aa8731efb
JayceQ <16831024@qq.com>
2019-02-17 18:12:50 +0800

    video on demand

---
+++ video_server/api/auth.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"golang-awesome/video_server/api/defs"
+	"golang-awesome/video_server/api/session"
+	"net/http"
+)
+
+var HEADER_FIELD_SESSION = "X-Session-Id"
+var HEADER_FIELD_UNAME = "X-User-Name"
+
+func validateUserSession(r *http.Request) bool {
+	sid := r.Header.Get(HEADER_FIELD_SESSION)
+	if len(sid) == 0 {
+		return false
+	}
+
+	uname, ok := session.IsSessionExpired(sid)
+	if ok {
+		return false
+	}
+
+	r.Header.Add(HEADER_FIELD_UNAME, uname)
+	return true
+}
+
+
+func ValidateUser(w http.ResponseWriter, r *http.Request) bool {
+	uname := r.Header.Get(HEADER_FIELD_UNAME)
+	if len(uname) == 0 {
+		sendErrorResponse(w, defs.ErrorNotAuthUser)
+		return false
+	}
+	return true
+}
---
+++ video_server/api/response.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"golang-awesome/video_server/api/defs"
+	"io"
+	"net/http"
+)
+
+func sendErrorResponse(w http.ResponseWriter, errResp defs.ErrResponse){
+	w.WriteHeader(errResp.HttpSC)
+	resStr, _ := json.Marshal(&errResp.Error)
+	io.WriteString(w, string(resStr))
+}
+
+func sendNormalResponse(w http.ResponseWriter, resp string, sc int) {
+	w.WriteHeader(sc)
+	io.WriteString(w, resp)
+}
