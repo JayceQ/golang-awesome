f8defebc46c64840ac23b6c8f5fa44d48129cfd1
JayceQ <16831024@qq.com>
2019-02-20 21:02:16 +0800

    video on demand

--- video_server/api/handlers.go
+++ video_server/api/handlers.go
@@ -1,17 +1,206 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
-	"io"
+	"golang-awesome/video_server/api/dbops"
+	"golang-awesome/video_server/api/defs"
+	"golang-awesome/video_server/api/session"
+	"golang-awesome/video_server/api/utils"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
-func CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params){
-	io.WriteString(w,"Create User Handler")
+func CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	//首先从request里面读出create user 相关的body
+	res, _ := ioutil.ReadAll(r.Body)
+	ubody := &defs.UserCredential{}
+
+	if err := json.Unmarshal(res, ubody); err != nil {
+		sendErrorResponse(w, defs.ErrorRequestBodyParseFailed)
+		return
+	}
+
+	if err := dbops.AddUserCredential(ubody.UserName, ubody.Pwd); err != nil {
+		sendErrorResponse(w, defs.ErrorDBError)
+	}
+
+	id := session.GenerateNewSessionId(ubody.UserName)
+	su := &defs.SignedUp{Success: true, SessionId: id}
+
+	if resp, err := json.Marshal(su); err != nil {
+		sendErrorResponse(w, defs.ErrorInternalFaults)
+		return
+	} else {
+		sendNormalResponse(w, string(resp), 201)
+	}
 }
 
-func UserInfo(w http.ResponseWriter, r *http.Request, p httprouter.Params){
-	name := p.ByName("name")
-	//s := fmt.Sprintf("Login user is %s", name)
-	io.WriteString(w,name)
+func Login(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	res, _ := ioutil.ReadAll(r.Body)
+	log.Printf("%s", res)
+	ubody := &defs.UserCredential{}
+
+	if err := json.Unmarshal(res, ubody); err != nil {
+		log.Panicf("%s", err)
+		sendErrorResponse(w, defs.ErrorRequestBodyParseFailed)
+		return
+	}
+
+	uname := params.ByName("username")
+	log.Panicf("login url name: %s", uname)
+	log.Printf("login body name: %s", ubody.UserName)
+	if uname != ubody.UserName {
+		sendErrorResponse(w, defs.ErrorNotAuthUser)
+		return
+	}
+
+	log.Printf("%s", ubody.UserName)
+	pwd, err := dbops.GetUserCredential(ubody.UserName)
+	log.Printf("login pwd: %s", pwd)
+	log.Printf("login body pwd: %s", ubody.Pwd)
+	if err != nil || len(pwd) == 0 || pwd != ubody.Pwd {
+		sendErrorResponse(w, defs.ErrorNotAuthUser)
+		return
+	}
+
+	id := session.GenerateNewSessionId(ubody.UserName)
+	si := &defs.SignedIn{Success: true, SessionId: id}
+	if resp, err := json.Marshal(si); err != nil {
+		sendErrorResponse(w, defs.ErrorInternalFaults)
+	} else {
+		sendNormalResponse(w, string(resp), 200)
+	}
+}
+
+func GetUserInfo(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	if !ValidateUser(w, r) {
+		log.Printf("unauthorized user\n")
+		return
+	}
+
+	uname := p.ByName("username")
+	u, err := dbops.GetUser(uname)
+	if err != nil {
+		log.Printf("error in GetUserInfo: %s", err)
+		sendErrorResponse(w, defs.ErrorDBError)
+		return
+	}
+
+	ui := defs.UserInfo{Id: u.Id}
+	if resp, err := json.Marshal(ui); err != nil {
+		sendErrorResponse(w, defs.ErrorInternalFaults)
+	} else {
+		sendNormalResponse(w, string(resp), 200)
+	}
+}
+
+func AddNewVideo(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	if !ValidateUser(w, r) {
+		log.Printf("unauthorized user\n")
+		return
+	}
+
+	res, _ := ioutil.ReadAll(r.Body)
+	nvbody := &defs.NewVideo{}
+	if err := json.Unmarshal(res, nvbody); err != nil {
+		sendErrorResponse(w, defs.ErrorRequestBodyParseFailed)
+		return
+	}
+
+	vi, err := dbops.AddNewVideo(nvbody.AuthorId, nvbody.Name)
+	log.Printf("author id: %d, name: %s \n", nvbody.AuthorId, nvbody.Name)
+	if err != nil {
+		log.Printf("error in AddNewVideo: %s", err)
+		sendErrorResponse(w, defs.ErrorDBError)
+		return
+	}
+
+	if resp, err := json.Marshal(vi); err != nil {
+		sendErrorResponse(w, defs.ErrorInternalFaults)
+	} else {
+		sendNormalResponse(w, string(resp), 201)
+	}
+}
+
+func ListAllVideos(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	if !ValidateUser(w, r) {
+		log.Printf("unauthirized user\n")
+		return
+	}
+
+	uname := p.ByName("username")
+	vs, err := dbops.ListVideoInfo(uname, 0, utils.GetCurrentTimestampSec())
+	if err != nil {
+		log.Printf("error in ListAllVideos: %s", err)
+		sendErrorResponse(w, defs.ErrorDBError)
+		return
+	}
+
+	vsi := &defs.VideosInfo{Videos: vs}
+	if resp, err := json.Marshal(vsi); err != nil {
+		sendErrorResponse(w, defs.ErrorInternalFaults)
+	} else {
+		sendNormalResponse(w, string(resp), 200)
+	}
+}
+
+func DeleteVideo(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	if !ValidateUser(w, r) {
+		return
+	}
+
+	vid := p.ByName("vid-id")
+	err := dbops.DeleteVideoInfo(vid)
+	if err != nil {
+		log.Printf("error in DeleteVideo: %s", err)
+		sendErrorResponse(w, defs.ErrorDBError)
+		return
+	}
+
+	go utils.SendDeleteVideoRequest(vid)
+	sendNormalResponse(w, "", 204)
+}
+
+func PostComment(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	if !ValidateUser(w, r) {
+		return
+	}
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	cbody := &defs.NewComment{}
+	if err := json.Unmarshal(reqBody, cbody); err != nil {
+		log.Printf("%s", err)
+		sendErrorResponse(w, defs.ErrorRequestBodyParseFailed)
+		return
+	}
+	vid := p.ByName("vid-id")
+	if err := dbops.AddNewComments(vid, cbody.AuthorId, cbody.Content); err != nil {
+		log.Printf("%s", err)
+		sendErrorResponse(w, defs.ErrorDBError)
+	} else {
+		sendNormalResponse(w, "ok", 201)
+	}
+
+}
+
+func ShowComments(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	if !ValidateUser(w, r) {
+		return
+	}
+
+	vid := p.ByName("vid-id")
+	cm, err := dbops.ListComments(vid, 0, utils.GetCurrentTimestampSec())
+	if err != nil {
+		log.Printf("error in ShowComments: %s", err)
+		sendErrorResponse(w, defs.ErrorDBError)
+		return
+	}
+
+	cms := &defs.Comments{Comments: cm}
+	if resp, err := json.Marshal(cms); err != nil {
+		sendErrorResponse(w, defs.ErrorInternalFaults)
+	} else {
+		sendNormalResponse(w, string(resp), 200)
+	}
 }
